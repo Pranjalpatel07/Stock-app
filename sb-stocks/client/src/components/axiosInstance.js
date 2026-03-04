1f384c073e35abdc1d322d7ec4340b638cce02a2
Prayag Sahu <sahuprayag229@gmail.com>
2026-03-04 11:42:33 +0530

    update

--- sb-stocks/client/src/components/axiosInstance.js
+++ sb-stocks/client/src/components/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL ,
+  baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
 });
 
